dd8a648bae68c7948dff4f785a663e2cebe22be8
Naveen singh <bunnynaveen363@gmail.com>
2026-08-11 15:07:55 +0530

    Time: 13 ms (99.94%) | Memory: 42.2 MB (17.03%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -5,10 +5,10 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int l = 1;
         int r = n;
-        while(l <= r){
+        while(l < r){
             int mid = l+(r-l)/2;
             if(isBadVersion(mid)){
-                r = mid -1 ;
+                r = mid ;
             }else {
                 l = mid+1;
             }
