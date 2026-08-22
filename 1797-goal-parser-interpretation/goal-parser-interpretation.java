f64413413469c78ac0532b8a935ea55dcee8ecaa
Naveen singh <bunnynaveen363@gmail.com>
2026-08-22 18:22:17 +0530

    Time: 1 ms (74.40%) | Memory: 43 MB (51.29%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,18 +1,7 @@
 class Solution {
     public String interpret(String c) {
-        StringBuilder s = new StringBuilder();
-
-        for(int i = 0;i < c.length();i++){
-            if(c.charAt(i) == '(' && c.charAt(i+1) ==')'){
-                s.append("o");
-                i++;
-            }else if(c.charAt(i) == 'G'){
-                s.append("G");
-            }else{
-                s.append("al");
-                i+=3;
-            }
-        }
-        return new String(s);
+        return c
+                .replace("()","o")
+               .replace("(al)","al");
     }
 }
